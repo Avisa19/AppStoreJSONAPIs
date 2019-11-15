540907f1318daca923ec8bba0f68ec373d321368
Avisa Poshtkouhi <avisa_poshtkouhi@yahoo.com>
2019-11-15 17:49:15 +1100

    iTunes Search API JSON Fetch.

--- AppStoreJSONAPIs/Controllers/AppsSearchController.swift
+++ AppStoreJSONAPIs/Controllers/AppsSearchController.swift
@@ -18,6 +18,35 @@ class AppsSearchController: UICollectionViewController {
         collectionView.backgroundColor = .white
         self.collectionView!.register(SearchAppCell.self, forCellWithReuseIdentifier: searchIdentifier)
         
+        fetchiTunesApps()
+    }
+    
+    fileprivate func fetchiTunesApps() {
+        
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        // fetch data from internet
+        URLSession.shared.dataTask(with: url) { (data, respo, err) in
+            if let err = err {
+                print("Failed to fetch data from internet:", err)
+                return
+            }
+            
+            // success
+            guard let data = data else { return }
+            do {
+                
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+//                print(searchResult)
+                searchResult.results.forEach({print($0.trackName)})
+            
+                
+            } catch let jsonErr {
+                print("Failed to fetch json Url:", jsonErr)
+            }
+            
+        }.resume() // fires off the request
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
---
+++ AppStoreJSONAPIs/Models/SearchResult.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResult.swift
+//  AppStoreJSONAPIs
+//
+//  Created by Avisa Poshtkouhi on 15/11/19.
+//  Copyright © 2019 Avisa Poshtkouhi. All rights reserved.
+//
+
+import Foundation
+
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
